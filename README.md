7c7d21ca0d4752a42594fa29d2911f9d35eb4850
Florian Schüller <florian.schueller@gmail.com>
2020-05-08 11:37:25 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,17 @@
+# XFCE Display Profiles for LightDM
+
+This script "exports" all XFCE Display Profiles to be used outside of XFCE e.g. with LightDM
+
+usage:
+```
+./xfconf_to_xrandr.sh > lightdm-display-setup-script.sh
+chmod a+x lightdm-display-setup-script.sh
+```
+
+you can also just test by calling the generated script then:
+´´´
+./lightdm-display-setup-script.sh
+´´´
+
+Then you can use this script as LightDM "display-setup-script"
+see https://github.com/Canonical/lightdm/blob/master/data/lightdm.conf
---
+++ xfconf_to_xrandr.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+DISP_CONFS=$(xfconf-query -c displays -l |grep -Po "^/[^/]+(?=/)"|sort -u)
+
+get_name() {
+	xfconf-query -c displays -p $1 2>/dev/null |tr -d '\n' && echo " ($1)"|| echo "$1"
+}
+
+get_connected() {
+	xfconf-query -c displays -p $1 -l|grep -Po "(?<=^$1/)[^/]+$" | sort -u
+}
+
+is_active() {
+	if [[ $(xfconf-query -c displays -p $1/$2/Active) == true ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+get_resolution() {
+	xfconf-query -c displays -p $1/$2/Resolution |tr -d '\n'
+}
+get_pos() {
+	xfconf-query -c displays -p $1/$2/Position/X |tr -d '\n'
+	echo -n "x"
+	xfconf-query -c displays -p $1/$2/Position/Y |tr -d '\n'
+}
+
+get_rotate() {
+	ROTATION="$(xfconf-query -c displays -p $1/$2/Rotation)"
+	case "$ROTATION" in
+		90)
+			echo "left"
+		;;
+		180)
+			echo "inverted"
+		;;
+		270)
+			echo "right"
+		;;
+		*)
+			echo "normal"
+		;;
+	esac
+}
+
+echo 'CONNECTED="$(xrandr |grep -Po "^.*(?= connected)")"'
+
+for C in $DISP_CONFS; do
+	CMD="xrandr"
+	echo "# $(get_name $C)" 
+	echo "# Connected / Active-setting:"
+	DISP_CONN="$(get_connected $C)"
+	THE_IF="if true"
+	for D in $DISP_CONN; do
+		THE_IF="$THE_IF && [[ \$CONNECTED == *${D}* ]]"
+		if is_active $C $D; then
+			CMD="$CMD --output $D --mode $(get_resolution $C $D) --pos $(get_pos $C $D) --rotate $(get_rotate $C $D)"
+			echo "# - $D: Yes"
+		else
+			CMD="$CMD --output $D --off"
+			echo "# - $D: No"
+		fi
+	done
+	echo "$THE_IF ; then"
+	echo "echo \"Activating $(get_name $C)\""
+	echo "$CMD"
+	echo "exit 0"
+	echo "fi"
+	echo ""
+done
+
